c4c2f4efe7e4b6eeeb4c16498366b25cf1ddeff5
Thies Gerken <thies@thiesgerken.de>
2017-08-22 14:15:26 +0200

    zsh autocompletion

---
+++ completions.zsh
@@ -0,0 +1,26 @@
+#compdef wavepi
+_wavepi() {
+  local -a commands
+  commands=(
+    '--help'
+    '--version'
+    '--export-config:generate config file and write to stdout (or --config [file])'
+    '--config:config file input/output'
+    '--config-format:export format (Text|LaTeX|Description|XML|JSON|ShortText)'
+  )
+
+  if (( CURRENT == 2 )); then
+    _describe -t commands 'commands' commands
+  fi
+
+  return 0
+}
+
+_wavepi
+
+# Put this file in ~/.zsh-completions and the following in ~/.zshrc:
+# # folder of all of your autocomplete functions
+#fpath=($HOME/.zsh-completions $fpath)
+# enable autocomplete function
+# autoload -U compinit
+# compinit
