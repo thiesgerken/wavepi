8c6d4918203f5fb1fbf7c1b507247b34cb625d7a
Thies Gerken <thies@thiesgerken.de>
2018-02-08 14:17:03 +0100

    mpirun

--- examples/run-mpi.sh
+++ examples/run-mpi.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# only check on root node
-if [[ -d $1 ]]; then
- echo "directory $1 exists"
- exit 2
-fi
-
-mpirun --bind-to none -np 2 --hostfile hostfile rm -R $1
 mpirun --bind-to none -np 2 --hostfile hostfile ./run.sh $1
