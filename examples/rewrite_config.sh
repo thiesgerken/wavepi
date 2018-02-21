9c9dd2a1e728a332a4be77e68ee1279278ae949e
Thies Gerken <thies@thiesgerken.de>
2018-02-21 16:43:42 +0100

    rewrite configs to work again

--- examples/rewrite_config.sh
+++ examples/rewrite_config.sh
@@ -2,11 +2,11 @@
 
 echo $1
 
-if [[ ! -f $1.cfg ]]; then
- echo "$1.cfg not found"
+if [[ ! -f $1 ]]; then
+ echo "$1 not found"
  exit 1
 fi
 
-../build/wavepi --export-config -c $1.cfg > $1.cfg.new
-cp $1.cfg.new $1.cfg
-rm $1.cfg.new
+../build/wavepi --export-config -c $1 > $1.new
+cp $1.new $1
+rm $1.new
