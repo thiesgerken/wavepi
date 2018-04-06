77e3a228e172e58a77f368864b278b13f96b66dc
Thies Gerken <thies@thiesgerken.de>
2018-04-06 10:44:34 +0200

    ogv

--- problems/copy_videos.sh
+++ problems/copy_videos.sh
@@ -9,4 +9,21 @@ for f in $(find $1 -name "reconstruction.ogv"); do
         cp $f $DEST
       fi
 
+      DEST="$(dirname $f)/../$(basename $(dirname $f)).ogv"
+
+      if [[ ! -f $DEST ]]; then
+        echo "$f -> $DEST"
+
+        cp $f $DEST
+      fi
+done
+
+for f in $(find $1 -name "estimate.ogv"); do
+      DEST="$(dirname $f)/../$(basename $(dirname $f)).ogv"
+
+      if [[ ! -f $DEST ]]; then
+        echo "$f -> $DEST"
+
+        cp $f $DEST
+      fi
 done
--- problems/make_videos.sh
+++ problems/make_videos.sh
@@ -4,7 +4,7 @@ for f in $(find $1 -wholename "*$2*"); do
   if [ "${f##*.}" == "pvd" ]; then
     if [[ ! -f ${f%.pvd}.ogv ]]; then
       echo $f
-      ./video2d.py $f $2 $3 $4 $5 $6
+      ./video2d.py $f $3 $4 $5 $6
     else
       echo "skipping $f"
     fi
