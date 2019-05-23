5cf35112a08472d6d94fbc394599cd54dae8cae3
Thies Gerken <thies@thiesgerken.de>
2019-05-23 13:27:15 +0200

    deal

--- docker/dealII.sh
+++ docker/dealII.sh
@@ -7,8 +7,8 @@ cd dealii
 git checkout v9.1.0
 mkdir build
 cd build
-cmake .. -DDEAL_II_WITH_MPI=ON -DCMAKE_INSTALL_PREFIX=/usr/local -DDEAL_II_COMPONENT_EXAMPLES=OFF
-make -j2
+cmake .. -DDEAL_II_WITH_MPI=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DDEAL_II_COMPONENT_EXAMPLES=OFF
+make -j16
 make install
 # keep a copy of the configuration log
 cp detailed.log /root/dealii.log
