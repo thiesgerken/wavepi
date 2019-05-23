e952795852f6bfea718766611c6a443ed852c759
Thies Gerken <thies@thiesgerken.de>
2019-05-23 13:38:45 +0200

    boost

--- docker/boost.sh
+++ docker/boost.sh
@@ -2,13 +2,13 @@
 
 echo "** Building & Installing Boost 1.62 **"
 cd /root
-curl https://netcologne.dl.sourceforge.net/project/boost/boost/1.62.0/boost_1_62_0.tar.gz -O
-tar xfz boost_1_62_0.tar.gz
-cd boost_1_62_0
+git clone https://github.com/boostorg/boost.git
+cd boost
+git checkout boost-1.62.0
 ./bootstrap.sh --prefix=/usr
 # deal.II must not find shared boost library, otherwise it will try to link dynamically to it!
 ./b2 cxxflags=-fPIC cflags=-fPIC -j16 variant=release link=static threading=multi runtime-link=static install
 
 echo "** Cleaning up **"
 cd /root
-rm -Rf boost_1_62_0 boost_1_62_0.tar.gz
+rm -Rf boost
