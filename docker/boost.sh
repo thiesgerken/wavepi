52380c534794dfb13cb93a4ff8ab187e3f7ed29f
Thies Gerken <thies@thiesgerken.de>
2019-05-23 12:16:27 +0200

    docker

--- docker/boost.sh
+++ docker/boost.sh
@@ -7,7 +7,7 @@ tar xfz boost_1_62_0.tar.gz
 cd boost_1_62_0
 ./bootstrap.sh --prefix=/usr
 # deal.II must not find shared boost library, otherwise it will try to link dynamically to it!
-./b2 cxxflags=-fPIC cflags=-fPIC -j2 variant=release link=static threading=multi runtime-link=static install
+./b2 cxxflags=-fPIC cflags=-fPIC -j16 variant=release link=static threading=multi runtime-link=static install
 
 echo "** Cleaning up **"
 cd /root
